281be45883ad417a47f058fbd9ff3078d2852404
rgreen32 <ray.greenm@gmail.com>
2022-10-13 19:13:51 -0500

    edit

--- test_scenes.py
+++ test_scenes.py
@@ -7,53 +7,6 @@ from manim.opengl import OpenGLSurface
 from ndarray_listener import ndl
 
 
-class GuassElimination(ThreeDScene):
-    rotation_matrix_X = [[1, 0, 0],
-                        [0, np.cos(np.pi/2), np.sin(np.pi/2)],
-                        [0, -np.sin(np.pi/2), np.cos(np.pi/2)]]
-
-    rotation_matrix_Y = [[np.cos(np.pi/2), 0, np.sin(np.pi/2)],
-                        [0, 1, np.sin(np.pi/2)],
-                        [-np.sin(np.pi/2), 0, np.cos(np.pi/2)]]
-
-    rotation_matrix_Z = [[np.cos(np.pi/2), -np.sin(np.pi/2), 0],
-                        [np.sin(np.pi/2), np.cos(np.pi/2), 0],
-                        [0, 0, 1]]    
-
-    matrix = np.array([[4, -4,  -1],
-                       [4,  12, -5],
-                       [4,  5,   6]])
-    vector = np.array([[-10],
-                       [14],
-                       [-15]])
-    planes = []
-
-    def construct(self):
-        self.renderer.camera.light_source.move_to(3*UP) # changes the source of the light
-        self.set_camera_orientation(phi=75 * DEGREES, theta=40 * DEGREES, center_point=[1.0, 1.0, 1.0])
-        self.begin_ambient_camera_rotation(.1)
-
-        axes = ThreeDAxes(x_axis_config={"color": BLUE}, y_axis_config={"color": RED}, z_axis_config={"color": GREEN})
-        self.add(axes)
-
-        colors = (BLUE, RED, GREEN)
-        uv_range = (-3, 0)
-        for row_index in range(self.matrix.shape[0]):
-            def uv_func(u, v):
-                uv = (self.vector[row_index][0] + (-1*self.matrix[row_index][0])*u + (-1*self.matrix[row_index][1])*v)/self.matrix[row_index][2]
-                point = np.array([u,    #x component
-                                  v,    #y component
-                                  uv if uv > uv_range[0] and uv < uv_range[1] else None])  #z component
-                return point
-            plane = OpenGLSurface(uv_func, color=colors[row_index], u_range=[-3, 1], v_range=[-1, 3])
-            self.planes.append(plane)
-        
-        self.add(*self.planes)
-        
-        
-        self.interactive_embed()
-
-
 class ObservableMatrix(np.ndarray):
     prev_matrix = None
     def __new__(self, input_array, onupdate_func = None):
@@ -72,7 +25,118 @@ class ObservableMatrix(np.ndarray):
             self.onupdate_func()
         self.prev_matrix = None
         return None
-        
+
+
+class GuassElimination(ThreeDScene):
+    rotation_matrix_X = [[1, 0, 0],
+                        [0, np.cos(np.pi/2), np.sin(np.pi/2)],
+                        [0, -np.sin(np.pi/2), np.cos(np.pi/2)]]
+
+    rotation_matrix_Y = [[np.cos(np.pi/2), 0, np.sin(np.pi/2)],
+                        [0, 1, np.sin(np.pi/2)],
+                        [-np.sin(np.pi/2), 0, np.cos(np.pi/2)]]
+
+    rotation_matrix_Z = [[np.cos(np.pi/2), -np.sin(np.pi/2), 0],
+                        [np.sin(np.pi/2), np.cos(np.pi/2), 0],
+                        [0, 0, 1]]    
+
+    matrix_row_planes = []
+
+    def construct(self):
+        self.renderer.camera.light_source.move_to(3*UP) # changes the source of the light
+        self.set_camera_orientation(phi=75 * DEGREES, theta=40 * DEGREES, center_point=[1.0, 1.0, 1.0])
+        # self.begin_ambient_camera_rotation(.1)
+
+        axes = ThreeDAxes(x_axis_config={"color": BLUE}, y_axis_config={"color": RED}, z_axis_config={"color": GREEN})
+        self.add(axes)
+        self.matrix = ObservableMatrix(np.array([[4.0, -4.0,  -1.0, -10.0],
+                                                 [4.0,  12.0, -5.0, 14.0],
+                                                 [4.0,  5.0,   6.0, -15.0]]), self._on_update)
+        colors = (BLUE, RED, GREEN)
+        uv_range = (-3, 0)
+        for row_index in range(self.matrix.shape[0]):
+            def uv_func(u, v):
+                uv = (self.matrix[row_index][3] + (-1*self.matrix[row_index][0])*u + (-1*self.matrix[row_index][1])*v)/self.matrix[row_index][2]
+                point = np.array([u,    #x component
+                                  v,    #y component
+                                  uv])  #z component
+                return point
+            plane = OpenGLSurface(uv_func, color=colors[row_index], u_range=[-100, 100], v_range=[-100, 100])
+            self.matrix_row_planes.append(plane)
+        self.add(*self.matrix_row_planes)
+
+        self.matrix_string = MarkupText(str(self.matrix))
+        self.matrix_string.rotate(np.pi/2, [1,0,0])
+        self.add(self.matrix_string)
+
+        self._scale_matrix_row(0.25, 0)
+        print(self.matrix)
+        print("==================")
+        self.wait(2)
+        self._pivot_matrix_row((0,0), 1)
+        print(self.matrix)
+        print("==================")
+        self.wait(2)
+        self._pivot_matrix_row((0,0), 2)
+        print(self.matrix)
+        print("==================")
+
+        self._scale_matrix_row((1.0/16.0), 1)
+        print(self.matrix)
+        print("==================")
+        self.wait(2)
+        self._pivot_matrix_row((1,1), 0)
+        # print(self.matrix)
+        # print("==================")
+        # self.wait(2)
+        # self._pivot_matrix_row(0, 2)
+        # print(self.matrix)
+        # print("==================")
+        self.interactive_embed()
+
+    def _scale_matrix_row(self, scale_factor, row_number):
+        self.matrix[row_number] = self.matrix[row_number] * scale_factor
+        # scale_matrix = np.identity(3) * scale_factor 
+        # self.play(self.matrix_row_planes[row_number].animate.apply_matrix(scale_matrix))
+
+    def _pivot_matrix_row(self, base_rowcoloumn: Tuple[int, int], target_row_number: int):
+        new_row_vector = self.matrix[target_row_number] + (self.matrix[base_rowcoloumn[0]] * (-1*self.matrix[target_row_number][base_rowcoloumn[1]]))
+        self.matrix[target_row_number] = new_row_vector
+        # pivot_matrix = np.identity(3)
+        # pivot_matrix[0][0] = new_row_vector[0]/self.matrix[target_row_number][0]
+        # pivot_matrix[1][1] = new_row_vector[1]/self.matrix[target_row_number][1]
+        # pivot_matrix[2][2] = new_row_vector[2]/self.matrix[target_row_number][2]
+        # self.matrix[target_row_number] = new_row_vector
+        # self.play(ApplyMatrix(pivot_matrix), self.matrix_row_planes[target_row_number], about_point=[0,0,0])
+        # self.play(self.matrix_row_planes[target_row_number].animate.apply_matrix(pivot_matrix))
+
+    def _on_update(self):
+        self._update_matrix_string()
+        self._update_row_planes()
+
+
+    def _update_row_planes(self):   #animating planes on pivot operation fails?
+        animations = []
+        for row_index in range(self.matrix.shape[0]): 
+            old_row_vector = self.matrix.prev_matrix[row_index].copy()
+            update_matrix = np.identity(3)
+            update_matrix[0][0] = self.matrix[row_index][0]/old_row_vector[0]
+            update_matrix[1][1] = self.matrix[row_index][1]/old_row_vector[1]
+            update_matrix[2][2] = self.matrix[row_index][2]/old_row_vector[2]
+            update_matrix = np.nan_to_num(update_matrix)
+            # update_matrix[0][0] = 0.0
+            # update_matrix[1][1] = 0.66666667
+            # update_matrix[2][2] = 0.0
+            animations.append(ApplyMatrix(update_matrix, self.matrix_row_planes[row_index], about_point=[0,0,0]))
+        self.play(*animations)
+
+    def _update_matrix_string(self):
+        self.remove(self.matrix_string)
+        self.matrix_string = MarkupText(str(self.matrix))
+        self.matrix_string.rotate(np.pi/2, [1,0,0])
+        self.add(self.matrix_string)
+
+
 class GuassEliminationVectors(ThreeDScene):
     vector = np.array([[-10],
                        [ 14],
